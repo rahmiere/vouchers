8bd8460ce9d91146e62e5ab037c418e27604cfd5
rahmiere <rhr@riseup.net>
2024-01-16 12:25:35 -0500

    assisted inject

--- src/main/java/org/killjoy/vouchers/menu/MenuFactory.java
+++ src/main/java/org/killjoy/vouchers/menu/MenuFactory.java
@@ -2,8 +2,9 @@ package org.killjoy.vouchers.menu;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.killjoy.vouchers.menu.impl.EditMenu;
+import org.killjoy.vouchers.voucher.Voucher;
 
 public interface MenuFactory {
 
-    @NonNull EditMenu editMenu();
+    @NonNull EditMenu editMenu(final Voucher voucher);
 }
--- src/main/java/org/killjoy/vouchers/menu/impl/EditMenu.java
+++ src/main/java/org/killjoy/vouchers/menu/impl/EditMenu.java
@@ -1,28 +1,33 @@
 package org.killjoy.vouchers.menu.impl;
 
 import com.google.inject.Inject;
-import org.bukkit.inventory.ItemStack;
+import com.google.inject.assistedinject.Assisted;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
 import org.incendo.interfaces.paper.type.ChestInterface;
 import org.killjoy.vouchers.language.LangKey;
 import org.killjoy.vouchers.language.Language;
 import org.killjoy.vouchers.menu.Menu;
+import org.killjoy.vouchers.voucher.Voucher;
+
+import java.util.Collections;
 
 @DefaultQualifier(NonNull.class)
 public final class EditMenu extends Menu {
 
     private final Language language;
+    private final Voucher voucher;
 
     @Inject
-    public EditMenu(Language language) {
+    public EditMenu(Language language, @Assisted Voucher voucher) {
         this.language = language;
+        this.voucher = voucher;
     }
 
     @Override
     protected ChestInterface build() {
         return ChestInterface.builder()
-                .title(language.get(LangKey.EDIT_MENU_TITLE))
+                .title(language.get(LangKey.EDIT_MENU_TITLE, Collections.singletonMap("key", voucher.getKey())))
                 .rows(1)
                 .build();
     }
