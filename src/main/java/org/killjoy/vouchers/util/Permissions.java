2fc80c2911203a8700c915f8db7a751c4d42c327
rahmiere <rhr@riseup.net>
2024-01-16 13:26:49 -0500

    perms

---
+++ src/main/java/org/killjoy/vouchers/util/Permissions.java
@@ -0,0 +1,12 @@
+package org.killjoy.vouchers.util;
+
+public final class Permissions {
+
+    private Permissions() {
+    }
+
+    public static final String CREATE_COMMAND = "vouchers.admin.create";
+    public static final String EDIT_COMMAND = "vouchers.admin.edit";
+    public static final String GIVE_COMMAND = "vouchers.admin.give";
+
+}
